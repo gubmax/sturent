e764078b484b1369c8842885ee058186fff9b2e7
Максим Губышев <maksim@macbook.local>
2019-03-31 22:29:30 +0300

    Fix render in clientRouter after loading bundle

--- src/client/routers/clientRouter.jsx
+++ src/client/routers/clientRouter.jsx
@@ -1,52 +1,85 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-// import Loadable from 'react-loadable';
+import { func, string } from 'prop-types'
 
 import routes from '../routes'
 import App from '../layouts/App/App'
 
 class AsyncComponent extends Component {
-    state = { LoadedComponent: null }
+    static propTypes = {
+      load: func.isRequired,
+      resolveId: string.isRequired,
+    }
+
+    constructor(props) {
+      super(props)
+
+      const { isWebpackReady } = this
+      const { resolveId } = props
+      let LoadedComponent = null
+
+      if (isWebpackReady(resolveId)) {
+        // eslint-disable-next-line no-undef
+        const module = __webpack_require__(resolveId)
+        LoadedComponent = module.default || module
+      }
+
+      this.state = { LoadedComponent }
+    }
 
     componentDidMount() {
-        const { load } = this.props
+      const { load } = this.props
+      const { LoadedComponent } = this.state
 
+      if (!LoadedComponent) {
         load().then((component) => {
-            this.setState({ LoadedComponent: component.default || component })
+          this.setState({ LoadedComponent: component.default || component })
         })
+      }
+    }
+
+    isWebpackReady = (moduleId) => {
+      // eslint-disable-next-line camelcase
+      if (typeof __webpack_modules__ !== 'object') {
+        return false
+      }
+
+      return (
+        typeof moduleId !== 'undefined'
+            // eslint-disable-next-line no-undef
+            && typeof __webpack_modules__[moduleId] !== 'undefined'
+      )
     }
 
     render() {
-        const { LoadedComponent } = this.state
+      const { LoadedComponent } = this.state
 
-        if (!LoadedComponent) return null
+      if (!LoadedComponent) return null
 
-        return <LoadedComponent {...this.props} />
+      return <LoadedComponent {...this.props} />
     }
 }
 
 const clientRouter = () => (
-    <App>
-        {
-            routes.map(route => (
-                <Route
-                    key={route.path}
-                    render={() => <AsyncComponent load={() => import(`../pages/${route.componentName}/${route.componentName}`)} />}
-                    {...route}
-                />
-            ))
-            // routes.map(props => {
-            //   props.component = Loadable({
-            //     loader: () => import(`../pages/${props.componentName}/${props.componentName}`),
-            //     loading: () => null,
-            //     delay: 200,
-            //     timeout: 10000,
-            //   })
-            //
-            //   return <Route key={props.path} {...props}/>
-            // })
-        }
-    </App>
+  <App>
+    {
+      routes.map((route) => {
+        const { componentName } = route
+        return (
+          <Route
+            key={route.path}
+            render={() => (
+              <AsyncComponent
+                load={() => import(`../pages/${componentName}/${componentName}`)}
+                resolveId={require.resolveWeak(`../pages/${componentName}/${componentName}`)}
+              />
+            )}
+            {...route}
+          />
+        )
+      })
+    }
+  </App>
 )
 
 export default clientRouter
