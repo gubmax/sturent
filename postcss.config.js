c9895c541d931157fae81fc5fe4600be5dcf70b8
Максим Губышев <maksim@macbook.local>
2019-04-28 18:34:06 +0300

    Fix stylelint problems

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,13 @@ module.exports = {
     plugins: {
         'postcss-import': {},
         'postcss-color-mod-function': {},
+        stylelint: {
+            configFilefigFile: './stylelintrc',
+        },
         'postcss-preset-env': {
             stage: 0,
             browsers: 'last 5 versions',
             autoprefixer: 'last 5 versions',
-        },
-        stylelint: {
-            configFilefigFile: './stylelintrc',
         },
     },
 }
